763ee53fa457d2331323c4c5cb6ae62b6ab70788
Erik <eeckee@gmail.com>
2018-12-21 16:39:12 +0100

    close option. Not working with two skills?

--- __init__.py
+++ __init__.py
@@ -3,32 +3,77 @@ from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 
 import os
+from os.path import join
+from subprocess import Popen
+import subprocess
+import daemon
+
 
 class TestSkill(MycroftSkill):
-
     def __init__(self):
         super(TestSkill, self).__init__(name="TestSkill")
         self.dialog = []
-        self.program_list = ['konsole',  'firefox', 'spotify']
+        self.program_list = [
+                'console',
+                'firefox',
+                'browser',
+                'spotify',
+                'signal',
+                'discover',
+                'slack',
+                'dolphin']
 
-    @intent_handler(IntentBuilder("").require("Testing"))
+    @intent_handler(IntentBuilder("").require("Close"))
     def handle_hello_world_intent(self, message):
-        utterance = message.data.get('utterance')
-        self.dialog.append(utterance)
+        program = message.data.get('utterance').lower()
+        success = False
 
-        for d in self.dialog:
-            self.speak_dialog(d)
+        for p in program.split():
+            if p in self.program_list:
+                if p == 'console':
+                    p = 'konsole'
+                elif p == 'folders':
+                    p = 'dolphin'
+                elif p == 'signal':
+                    p = 'signal-desktop'
+
+                subprocess.Popen(['pkill', p])  
+                # Only works one time?
+                # with daemon.DaemonContext():  
+                    # This process now survives mycroft termination
+                    # subprocess.Popen(p)  
+                self.speak_dialog('Done!')
+                success = True
+
+        if not success:
+            self.speak_dialog(f'Whats {program.split()[-1]}?')
+        return True
 
     @intent_handler(IntentBuilder("").require("Open"))
     def handle_hello_world_intent(self, message):
         program = message.data.get('utterance').lower()
+        success = False
 
         for p in program.split():
-            if p == 'console':
-                p = 'konsole' 
             if p in self.program_list:
-                self.speak_dialog('Opening ' + p)
-                os.system(p + ' &')
+                if p == 'console':
+                    p = ['konsole','-e', 'tmux']
+                elif p == 'folders':
+                    p = 'dolphin'
+                elif p == 'signal':
+                    p = 'signal-desktop'
+
+                subprocess.Popen(p)  
+                # Only works one time?
+                # with daemon.DaemonContext():  
+                    # This process now survives mycroft termination
+                    # subprocess.Popen(p)  
+                self.speak_dialog('Done!')
+                success = True
+
+        if not success:
+            self.speak_dialog(f'Whats {program.split()[-1]}?')
+        return True
 
     def stop(self):
        return False

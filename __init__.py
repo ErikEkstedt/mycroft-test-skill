4c435ad6ed03340da6142be9860f84818ef020db
Erik <eeckee@gmail.com>
2018-12-22 00:03:45 +0100

    Added directions for virtual desktops in kde

--- __init__.py
+++ __init__.py
@@ -3,10 +3,7 @@ from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
 
 import os
-from os.path import join
-from subprocess import Popen
 import subprocess
-import daemon
 
 
 class TestSkill(MycroftSkill):
@@ -24,6 +21,7 @@ class TestSkill(MycroftSkill):
                 'status',
                 'dolphin',
                 'folders']
+        self.directions = ['left', 'right', 'back', 'next']
 
     @intent_handler(IntentBuilder("").require("close"))
     def close_programs(self, message):
@@ -52,8 +50,10 @@ class TestSkill(MycroftSkill):
 
         for p in program.split():
             if p in self.program_list:
-                p = self.check_correct_process_name(p)
-                subprocess.Popen(p)  
+                p_list = self.check_correct_process_name(p)
+                
+                for p in p_list:
+                    subprocess.Popen(p)  
                 # Only works one time?
                 # with daemon.DaemonContext():  
                     # This process now survives mycroft termination
@@ -64,17 +64,35 @@ class TestSkill(MycroftSkill):
         if not success:
             self.speak_dialog(f'Whats {program.split()[-1]}?')
 
+    @intent_handler(IntentBuilder("").require("Go"))
+    def move_desktop(self, message):
+        def command(p):
+            if p in ['left', 'back', 'previous']:
+                cmd = ['qdbus',  'org.kde.KWin',
+                        '/KWin', 'org.kde.KWin.previousDesktop']
+            elif p in ['right', 'next']:
+                cmd = ['qdbus',  'org.kde.KWin', '/KWin', 'org.kde.KWin.nextDesktop']
+            return cmd
+
+        program = message.data.get('utterance').lower()
+        success = False
+        for p in program.split():
+            if p in self.directions:
+                cmd = command(p)
+                subprocess.Popen(cmd)  
+
     def check_correct_process_name(self, p):
         if p == 'console':
-            p = 'konsole'
+            p = ['konsole']
         elif p == 'discover':
-            p = 'plasma-discover'
+            p = ['plasma-discover']
         elif p == 'folders':
-            p = 'dolphin'
+            p = ['dolphin']
         elif p == 'signal':
-            p = 'signal-desktop'
+            p = ['signal-desktop']
         elif p == 'status':
-            p = ['konsole', '-e',  'htop']
+            # p = ['konsole', '-e',  'htop', 'ksysguard']
+            p = [['konsole', '-e',  'htop'], ['ksysguard']]
         return p
 
     def stop(self):
